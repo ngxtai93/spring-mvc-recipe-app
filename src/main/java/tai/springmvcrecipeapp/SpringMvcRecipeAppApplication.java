26fd683ffe2dffcedf17005517ef0e5d2cd98459
Tai Nguyen <ng.xtai93@gmail.com>
2018-03-20 20:36:46 -0500

    First commit

---
+++ src/main/java/tai/springmvcrecipeapp/SpringMvcRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package tai.springmvcrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRecipeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/tai/springmvcrecipeapp/SpringMvcRecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package tai.springmvcrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcRecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
