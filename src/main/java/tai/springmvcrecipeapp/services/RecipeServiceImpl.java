2cd164eac9fb69e4339f655cefb72a0cd87ae534
Tai Nguyen <ng.xtai93@gmail.com>
2018-03-23 18:07:21 -0500

    View Recipe function

--- src/main/java/tai/springmvcrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/tai/springmvcrecipeapp/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import tai.springmvcrecipeapp.domain.Recipe;
 import tai.springmvcrecipeapp.repositories.RecipeRepository;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -28,5 +29,16 @@ public class RecipeServiceImpl implements RecipeService {
             .forEach(recipes::add)
         ;
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long l) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if(!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+        return recipeOptional.get();
     }
 }
--- src/test/java/tai/springmvcrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/tai/springmvcrecipeapp/services/RecipeServiceImplTest.java
@@ -8,12 +8,12 @@ import tai.springmvcrecipeapp.domain.Recipe;
 import tai.springmvcrecipeapp.repositories.RecipeRepository;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -30,6 +30,21 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() {
 
         Recipe recipe = new Recipe();
@@ -42,5 +57,6 @@ public class RecipeServiceImplTest {
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 }
