0d18da9bcf55f0978e6d1c01868c624529ea874a
Tai Nguyen <ng.xtai93@gmail.com>
2018-04-18 13:55:01 -0500

    Create a Recipe

--- src/main/java/tai/springmvcrecipeapp/controllers/RecipeController.java
+++ src/main/java/tai/springmvcrecipeapp/controllers/RecipeController.java
@@ -2,8 +2,8 @@ package tai.springmvcrecipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import tai.springmvcrecipeapp.commands.RecipeCommand;
 import tai.springmvcrecipeapp.services.RecipeService;
 
 @Controller
@@ -19,5 +19,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
